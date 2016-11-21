721fc312a69446ed07089b0a3e6027027bf33f41
Juliette Rengot <juliette.rengot@eleves.enpc.fr>
2016-11-21 16:26:41 +0100

    Les pages sont liées

--- InterfaceGraphique/InterfaceGraphique.py
+++ InterfaceGraphique/InterfaceGraphique.py
@@ -5,6 +5,12 @@ from PyQt4 import QtGui, uic
 
 app = QtGui.QApplication(sys.argv)
 widget = uic.loadUi("mainwindow.ui")
+
+widget.RulesButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_2))
+widget.TwoPlayersButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_3))
+widget.OnePlayerButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_4))
+widget.ZeroPlayerButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_5))
+widget.playButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab))
 widget.show()
 
 app.exec_()
