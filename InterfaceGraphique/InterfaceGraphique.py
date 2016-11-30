5171015931d65d39a00518a92d07f17d3edb5190
CharlesAuguste <charles.auguste@ymail.com>
2016-12-01 00:38:32 +0100

    Encapsulation de l'interface graphique dans une classe

--- InterfaceGraphique/InterfaceGraphique.py
+++ InterfaceGraphique/InterfaceGraphique.py
@@ -4,23 +4,31 @@ import sys
 from PyQt4 import QtGui, uic
 from boardgame_ai_py import *
 
+
+class InterfaceGraphique(QtGui.QWidget):
+    def __init__(self):
+        super(InterfaceGraphique, self).__init__()
+        self.widget = uic.loadUi("mainwindow.ui")
+
+        self.widget.RulesButton.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab_2))
+        self.widget.RulesButtonTab3.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab_2))
+        self.widget.RulesButtonTab4.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab_2))
+        self.widget.RulesButtonTab5.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab_2))
+
+        self.widget.ComeBackButtonTab3.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab))
+        self.widget.ComeBackButtonTab4.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab))
+        self.widget.ComeBackButtonTab5.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab))
+        self.widget.playButton.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab))
+
+        self.widget.TwoPlayersButton.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab_3))
+        self.widget.OnePlayerButton.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab_4))
+        self.widget.ZeroPlayerButton.clicked.connect(lambda _ : self.widget.tabWidget.setCurrentWidget(self.widget.tab_5))
+
+        self.widget.show()
+
+
+
+
 app = QtGui.QApplication(sys.argv)
-widget = uic.loadUi("mainwindow.ui")
-
-widget.RulesButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_2))
-widget.RulesButtonTab3.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_2))
-widget.RulesButtonTab4.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_2))
-widget.RulesButtonTab5.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_2))
-
-widget.ComeBackButtonTab3.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab))
-widget.ComeBackButtonTab4.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab))
-widget.ComeBackButtonTab5.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab))
-widget.playButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab))
-
-widget.TwoPlayersButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_3))
-widget.OnePlayerButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_4))
-widget.ZeroPlayerButton.clicked.connect(lambda _ : widget.tabWidget.setCurrentWidget(widget.tab_5))
-
-widget.show()
-
+InterfaceGraphique()
 app.exec_()
