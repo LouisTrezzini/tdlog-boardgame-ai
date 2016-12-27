4c20394db338961d1b15c21ef86d66f16a54879b
Juliette Rengot <juliette.rengot@eleves.enpc.fr>
2016-12-27 23:49:11 +0100

    normalisation des noms des variables

---
+++ cmake-build-debug/CMakeFiles/boardgame_ai_py.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boardgame_ai_py.dir/python-module.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/utils/MinMaxOutput.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/players/AlphaBetaPlayer.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/players/RandomPlayer.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/game/Board.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/game/Game.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/game/Color.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/players/MonteCarloTreeSearchPlayer.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/game/Node.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/players/HumanPlayer.cpp.o"
+  "CMakeFiles/boardgame_ai_py.dir/players/MinMaxPlayer.cpp.o"
+  "boardgame_ai_py.pdb"
+  "boardgame_ai_py.cpython-35m-x86_64-linux-gnu.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boardgame_ai_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
